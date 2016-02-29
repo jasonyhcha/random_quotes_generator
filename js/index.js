0299632e24f6885a00cc19c1b7f735a8f25ba48f
Jason Cha <Jason Cha>
2016-02-29 15:25:40 -0800

    Added random quote functionality

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-var parsed;
+var quote, author;
 
 $("#generate").click(function(){
-  $.getJSON("https://andruxnet-random-famous-quotes.p.mashape.com/", function(a) {
-    $("#quote").append(a.quote);
+  $.ajax({
+    beforeSend: function(request) {
+        request.setRequestHeader("X-Mashape-Key", '6zc8VGkISCmshHBGJckjSvnEe2kkp1Nr8NYjsn3NBOG9mEgj4k');
+    },
+    dataType: "json",
+    url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+    success: function(result) {
+      quote = result.quote;
+      author = result.author;
+      $("#quote").html('"' + quote+'"');
+      $("#author").html("-"  + author);
+    }
   });
-})
+});
